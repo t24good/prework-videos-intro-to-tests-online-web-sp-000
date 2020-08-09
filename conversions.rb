4b9f29f14b0337f80a9a87ff53eda954e98c7a76
Tanesha Goodman <t24good@icloud.com>
2020-08-09 20:24:55 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -1,4 +1,4 @@
-GRAMS_PER_OUNCE = 28.3495
-def ounces_to_grams(ounces)
-  ounces.to_f * GRAMS_PER_OUNCE
+GRAMS_STATIC = 28.3495
+def ounces_to_grams(incoming_value)
+  incoming_value.to_f * GRAMS_STATIC
 end
